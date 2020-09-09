8a6df4a756c076afc30050df862e3e089e06c308
kimd345 <dong_hyuk_kim@alumni.brown.edu>
2020-09-09 14:07:02 -0400

    Work on backend package management

--- database.py
+++ database.py
@@ -1,25 +1,16 @@
 from dotenv import load_dotenv
-load_dotenv()
-
 from starter_app import app, db
 from starter_app.models import User
 
+load_dotenv()
+
+
 with app.app_context():
-  db.drop_all()
-  db.create_all()
+    db.drop_all()
+    db.create_all()
 
-  ian = User(username = 'Ian', email = 'ian@aa.io')
-  javier = User(username = 'Javier', email = 'javier@aa.io')
-  dean = User(username = 'Dean', email = 'dean@aa.io')
-  angela = User(username = 'Angela', email = 'angela@aa.io')
-  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io')
-  alissa = User(username = 'Alissa', email = 'alissa@aa.io')
+    demo_user = User(email='user@demo.com')
 
-  db.session.add(ian)
-  db.session.add(javier)
-  db.session.add(dean)
-  db.session.add(angela)
-  db.session.add(soonmi)
-  db.session.add(alissa)
+    db.session.add(demo_user)
 
-  db.session.commit()
+    db.session.commit()
--- starter_app/models.py
+++ starter_app/models.py
@@ -2,16 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
