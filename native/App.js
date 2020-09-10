4aec175d6e63fd784fcafebfdd8abff80146f164
kimd345 <dong_hyuk_kim@alumni.brown.edu>
2020-09-10 11:44:33 -0400

    Flesh out Welcome page

--- native/App.js
+++ native/App.js
@@ -1,21 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>SHIBAL</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
