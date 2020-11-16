eda5399d4ec8cca960eb4481fc3b46bc97c6a98e
kimd345 <dong_hyuk_kim@alumni.brown.edu>
2020-11-16 18:17:22 -0500

    Configure environments in app client

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,10 @@
 import { create } from 'apisauce';
 import cache from '../utility/cache';
 import authStorage from '../auth/storage';
+import settings from '../config/settings';
 
 const apiClient = create({
-  baseURL: 'http://192.168.1.41:5000/api',
+  baseURL: settings.apiUrl,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
