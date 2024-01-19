14faa25dabea4f03151f01618bcae4a2b132298b
Dan Kim <kimd345@gmail.com>
2024-01-19 13:41:15 -0500

    Fix custom font config

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import { AppLoading } from 'expo';
-import { LogBox } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import AuthNavigator from './src/navigation/AuthNavigator';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -12,38 +13,52 @@ import AuthContext from './src/auth/context';
 import authStorage from './src/auth/storage';
 import store from './src/redux/store';
 
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'DogeSans-Regular': require('./src/assets/fonts/DogeSans-Regular.otf'),
-    Osake: require('./src/assets/fonts/Osake.otf'),
-  });
-  const [isReady, setIsReady] = useState(false);
-  const [user, setUser] = useState();
+	const [fontsLoaded, fontError] = useFonts({
+		'DogeSans-Regular': require('./src/assets/fonts/DogeSans-Regular.otf'),
+		Osake: require('./src/assets/fonts/Osake.otf'),
+	});
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, fontError]);
 
-  if (!fontsLoaded) return <AppLoading />;
+	const [isReady, setIsReady] = useState(false);
+	const [user, setUser] = useState();
 
-  if (!isReady) {
-    const restoreUser = async () => {
-      const user = await authStorage.getUser();
-      if (user) setUser(user);
-    };
-    restoreUser().then(setIsReady(true));
-  }
+	if (!fontsLoaded || fontError) return null;
 
-  LogBox.ignoreAllLogs();
+	if (!isReady) {
+		const restoreUser = async () => {
+			const user = await authStorage.getUser();
+			if (user) setUser(user);
+		};
+		restoreUser().then(setIsReady(true));
+	}
 
-  return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <Provider store={store}>
-        <OfflineNotice />
-        <NavigationContainer>
-          {user ? <AppNavigator /> : <AuthNavigator />}
-        </NavigationContainer>
-      </Provider>
-    </AuthContext.Provider>
-  );
+	return (
+		<View
+			style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+			onLayout={onLayoutRootView}
+		>
+      {/* TODO: configure status bar according to theme */}
+      <StatusBar style='auto' />
+      <Text>Open up App.js to start working on your app!</Text>
+			<AuthContext.Provider value={{ user, setUser }}>
+				<Provider store={store}>
+					<OfflineNotice />
+					<NavigationContainer>
+						{user ? <AppNavigator /> : <AuthNavigator />}
+					</NavigationContainer>
+				</Provider>
+			</AuthContext.Provider>
+		</View>
+	);
 }
